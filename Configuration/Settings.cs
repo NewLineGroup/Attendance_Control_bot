f39ec10dd2cd04ed3b53cc91c8f25e830e481769
Developer <golibjonabdurasulov47@gmail.com>
2024-02-26 02:45:13 -0800

    change bot token

--- Configuration/Settings.cs
+++ Configuration/Settings.cs
@@ -2,6 +2,6 @@ namespace AttendanceControlBot.Configuration;
 
 public static class Settings
 {
-    public static string botToken = "6354883403:AAHUjSvJgCoz1yx4kGUnbByxymhe4ObasMk";
+    public static string botToken = "6589140111:AAFOowhR6XZmOTWt0kSYYbgW9IFt1Vp5_-8";
     //public static string dbConnectionString = "host=213.230.65.55;port=5444;username=postgres;password=159357Dax;database=tcb_app";
 }
